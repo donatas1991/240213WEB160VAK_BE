f38c4331c306d6201e0b35e1628b11dba4e9e7e5
donatas1991 <donatasskardinskas@gmail.com>
2024-04-11 22:03:17 +0300

    sql

---
+++ paskaitos/php_09/books/perziureti.php
@@ -0,0 +1,46 @@
+<?php include '../parts/header.php'; ?>
+<?php include '../parts/navbar.php'; ?>
+
+<?php 
+
+    $bookId = $_GET['id'];
+
+    $sql = "SELECT * FROM `knygos` WHERE `id` = $bookId";
+    $result = $conn->query($sql);
+
+    $bookData = $result->fetch_assoc();
+
+?>
+
+        <header class="bg-light text-dark pt-5 pb-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1 class="display-1">Knygos "<?php echo $bookData['pavadinimas']; ?>" informacija</h1>
+                        <p class="lead">Informacija apie knygą</p>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+       
+
+
+        <div class="content pt-5 pb-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <p><strong>Knygos pavadinimas: </strong> <?php echo $bookData['pavadinimas']; ?></p>
+                        <p><strong>Autorius: </strong> Kolkas palikime</p>
+                        <p><strong>Puslapių sk.: </strong> <?php echo $bookData['puslapiu_skaicius']; ?></p>
+                        <p><strong>Kaina: </strong> <?php echo $bookData['kaina']; ?> €</p>
+                        <p>
+                            <a href="<?php echo $linksBaseDir; ?>/books/atnaujinti.php" class="btn btn-warning">Atnaujinti</a>
+                            <a href="<?php echo $linksBaseDir; ?>/books/salinti.php" class="btn btn-danger">Šalinti</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<?php include '../parts/footer.php'; ?>
---
+++ paskaitos/php_09/parts/header.php
@@ -0,0 +1,18 @@
+<?php 
+
+    $includeBaseDir = 'C:/xampp/htdocs/240213WEB160VAK_BE/paskaitos/php_09'; 
+    $linksBaseDir = 'https://localhost/240213WEB160VAK_BE/paskaitos/php_09'; 
+?>
+
+<?php include "$includeBaseDir/database.php"; ?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <title>PHP ir MYSQL 1</title>
+    </head>
+    
+    <body>
